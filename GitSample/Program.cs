ef9d856ec67f1cecf19a2b36e1765f9b5086ff27
sinanozhan <sinanozhan@users.noreply.github.com>
2017-09-12 22:55:44 +0300

    first changes

--- GitSample/Program.cs
+++ GitSample/Program.cs
@@ -9,7 +9,11 @@ namespace GitSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            var person = new Person();
+            person.ID = 5;
+            person.Name = "Andy";
+
+            Console.WriteLine("Hello world from the person that name is {0} and ID is {1}", person.Name, person.ID);
 
         }
     }
